d838c4f182299109fa2f3ceba7c0163f08859d47
horsjaco117 <jacob_horsley@live.com>
2025-02-16 20:36:50 -0700

    A big jumble of code

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -2,6 +2,9 @@
 Imports System
 
 Module MessageProgram
+    Private messages As New List(Of String)() 'Stored message
+
+
     Sub Main(args As String())
         'uncomment to test interactively
         'Test.Manual()
@@ -9,22 +12,40 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Dim message As String() = {"Hello", "Good bye", "Jimmy likes pizza!!", "too many bananas", "more", "aardvark", "must be a number", "I need one more message"}
-        Return "" 'messages
-        UserMessages("Hello", False) 'return
-        UserMessages("Goodbye", False)
-        Dim result As String
 
-        ' Adding messages sequentially
-        result = UserMessages("Hello", False)
-        result = UserMessages("Good bye", False)
-        result = UserMessages("Jimmy likes pizza!!", False)
 
-        Console.WriteLine(result) ' Expected output:
+        'All stored messages are deleted when clearMessage is true
+        If clear Then
+            messages.Clear()
+        End If
+
+
+
+        If Not String.IsNullOrEmpty(newMessage) AndAlso Not clear Then
+            messages.Add(newMessage)
+
+        End If
+
+
+        ''your code here
+        'Dim message As String() = {"Hello", "Good bye", "Jimmy likes pizza!!", "too many bananas", "more", "aardvark", "must be a number", "I need one more message"}
+        'Return "" 'messages
+        'UserMessages("Hello", False) 'return
+        'UserMessages("Goodbye", False)
+        'Dim result As String
+
+        '' Adding messages sequentially
+        'result = UserMessages("Hello", False)
+        'result = UserMessages("Good bye", False)
+        'result = UserMessages("Jimmy likes pizza!!", False)
+
+        'Console.WriteLine(result) ' Expected output:
         ' Hello
         ' Good bye
         ' Jimmy likes pizza!!
+
+        Return String.Join(vbNewLine, messages) & If(messages.Count > 0, vbNewLine, "")
+
 
     End Function
 
