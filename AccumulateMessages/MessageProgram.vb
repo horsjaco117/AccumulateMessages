c188815ffa21ebfa86d24feaee077e5101650f10
horsjaco117 <jacob_horsley@live.com>
2025-02-16 19:15:22 -0700

    test

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -10,7 +10,22 @@ Module MessageProgram
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
         'your code here
-        Return messages
+        Dim message As String() = {"Hello", "Good bye", "Jimmy likes pizza!!", "too many bananas", "more", "aardvark", "must be a number", "I need one more message"}
+        Return "" 'messages
+        UserMessages("Hello", False) 'return
+        UserMessages("Goodbye", False)
+        Dim result As String
+
+        ' Adding messages sequentially
+        result = UserMessages("Hello", False)
+        result = UserMessages("Good bye", False)
+        result = UserMessages("Jimmy likes pizza!!", False)
+
+        Console.WriteLine(result) ' Expected output:
+        ' Hello
+        ' Good bye
+        ' Jimmy likes pizza!!
+
     End Function
 
 
